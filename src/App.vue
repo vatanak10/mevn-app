e0a10e91fa4fc10ed4250ff01e535e8a9e7d1530
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-10-22 08:32:32 +0700

    :pencil2: add fundamental components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <navbar/>
+    <div id="app-container">
+      <router-view/>
+    </div>
+    <Footer/>
   </div>
-  <router-view/>
 </template>
 
+<script>
+  import navbar from '@/components/navbar.vue'
+  import Footer from '@/components/footer.vue'
+
+  export default{
+    name: 'mevn-app',
+    components:{
+      navbar,
+      Footer
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  @import './assets/css/style.css';
+  @import '~bootstrap/dist/css/bootstrap.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 
 createApp(App).use(store).use(router).mount('#app')
