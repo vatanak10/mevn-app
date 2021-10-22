48c52b8d03d9f05cdb3460a7e6521a96907c32b7
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-10-22 19:16:54 +0700

    Revert ":pencil2: add fundamental components"
    This reverts commit e0a10e91fa4fc10ed4250ff01e535e8a9e7d1530.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
-  <div id="app">
-    <navbar/>
-    <div id="app-container">
-      <router-view/>
-    </div>
-    <Footer/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-  import navbar from '@/components/navbar.vue'
-  import Footer from '@/components/footer.vue'
-
-  export default{
-    name: 'mevn-app',
-    components:{
-      navbar,
-      Footer
-    }
-  }
-</script>
-
 <style>
-  @import './assets/css/style.css';
-  @import '~bootstrap/dist/css/bootstrap.css';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
