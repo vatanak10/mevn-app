3008ac154eb2de46a3c32cb24ce830375e65dbe3
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-10-23 14:21:14 +0700

    :page_facing_up: add footer

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,18 @@
   <div id="app-container">
     <router-view/>
   </div>
+  <Footer/>
 </template>
 
 <script>
   import Navbar from '@/components/Navbar.vue'
+  import Footer from '@/components/Footer.vue'
 
   export default {
     name: 'app'  ,
     components: {
-      Navbar
+      Navbar,
+      Footer
     }
   }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer id="custom-footer">
+        <span class="mr-4">&copy; 2021 Koeurn Serey Vatanak</span>
+        <span class="mr-4">Private</span>
+        <span>Terms of Service</span>
+    </footer>
+</template>
