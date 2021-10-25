e383437adced08dabcbb292d010646c4a45dfa7c
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-10-25 19:20:32 +0700

    :passport_control: add status code

--- server/api/task/tasks-controller.js
+++ server/api/task/tasks-controller.js
@@ -1,24 +1,24 @@
 export function index(req, res) {
     // find all tasks
-    return res.json();
+    return res.status(200).json();
 }
 
 export function create(req, res) {
     // create new task
-    return res.json();
+    return res.status(201).json();
 }
 
 export function update(req, res) {
     // update task
-    return res.json();
+    return res.status(204).json();
 }
 
 export function remove(req, res) {
     // delete task
-    return res.json();
+    return res.status(204).json();
 }
 
 export function show(req, res) {
     // get task by id
-    return res.json();
+    return res.status(200).json();
 }
