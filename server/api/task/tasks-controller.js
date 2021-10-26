6527940666c56970acd6b4b4c841a7d26cd5813f
vatanak10 <koeurnsereyvatanak18@kit.edu.kh>
2021-10-26 21:10:22 +0700

    :card_file_box: CRUD methods

--- server/api/task/tasks-controller.js
+++ server/api/task/tasks-controller.js
@@ -1,24 +1,105 @@
+import User from '../../model/user-model';
+import Task from '../../model/task-model';
+import moment from 'moment';
+
 export function index(req, res) {
     // find all tasks
-    return res.status(200).json();
+    Task.find({}, (error, tasks) => {
+        if (error) {
+            return res.status(500).json();
+        }
+        return res.status(200).json({ tasks: tasks });
+    }).populate('author', 'username', 'user');
+
+    // return res.status(200).json();
 }
 
 export function create(req, res) {
     // create new task
-    return res.status(201).json();
+    const id = 10;
+    User.findOne({ id: id }, (error, user) => {
+        if (error && !user) {
+            return res.status(500).json();
+        }
+        const task = new Task(req.body.task);
+        task.author = user._id;
+        task.dueDate = moment(task.dueDate);
+
+        task.save(error => {
+            if (error) {
+                return res.status(500).json();
+            }
+            return res.status(201).json();
+        });
+    });
+
+    // return res.status(201).json();
 }
 
 export function update(req, res) {
     // update task
-    return res.status(204).json();
+    
+    const id = 10;
+
+    User.findOne({ _id: id }, (error, user) => {
+        if (error) {
+            return res.status(500).json();
+        }
+        if (!user) {
+            return res.status(404).json();
+        }
+
+        const task = req.body.task;
+        task.author = user._id;
+        task.dueDate = moment(task.dueDate);
+
+        Task.findByIdAndUpdate({ _id: task._id }, task, error => {
+            if (error) {
+                return res.status(500).json();
+            }
+            return res.status(204).json();
+        });
+    });
+    
+    // return res.status(204).json();
 }
 
 export function remove(req, res) {
     // delete task
-    return res.status(204).json();
+    const id = 10;
+    Task.findOne({ _id: req.params._id }, (error, task) => {
+        if (error) {
+            return res.status(500).json();
+        }
+        if (!task) {
+            return res.status(404).json();
+        }
+        if (task.author._id.toString() != id) {
+            return res.status(403).json({ message: "Not Allowed to delete another user's task!"});
+        }
+
+        Task.deleteOne({ _id: req.params._id }, error => {
+            if (error) {
+                return res.status(500).json();
+            }
+            return res.status(204).json();
+        });
+    });
+
+    // return res.status(204).json();
 }
 
 export function show(req, res) {
     // get task by id
-    return res.status(200).json();
+    Task.findOne({ _id: req.params.id }, (error, task) => {
+        if (error) {
+            return res.status(500).json();
+        }
+        if (!task) {
+            return res.status(404).json();
+        }
+        return res.status(200).json({ task: task });
+    });
+
+    // return res.status(200).json();
 }
